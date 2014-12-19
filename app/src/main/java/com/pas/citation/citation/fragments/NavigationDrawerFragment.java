d469c07a7a2ff955d0b44f3f0b22a8a9437e0fbf
dodushka <nsueengonga@mail.ru>
2014-12-19 12:43:36 +0200

    fragment refactoring

--- app/src/main/java/com/pas/citation/citation/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/com/pas/citation/citation/fragments/NavigationDrawerFragment.java
@@ -108,22 +108,7 @@ public class NavigationDrawerFragment extends Fragment implements LoaderManager.
         adapter=new AuthorCursorAdapter(null,getActivity());
         drawerRecyclerView.setAdapter(adapter);
         getLoaderManager().initLoader(0,null,this);
-       /* drawerRecyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                selectItem(position);
-            }
-        });
-        drawerRecyclerView.setAdapter(new ArrayAdapter<String>(
-                getActionBar().getThemedContext(),
-                android.R.layout.simple_list_item_activated_1,
-                android.R.id.text1,
-                new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
-                }));
-        drawerRecyclerView.setItemChecked(mCurrentSelectedPosition, true);*/
+
 
         return drawerRecyclerView;
     }
